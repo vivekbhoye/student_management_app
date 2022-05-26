7fb4c8e7ac9391f87daeeadc59bc0520cb0c1563
vivekbhoye <vivekrbhoye@gmail.com>
2022-05-26 22:10:09 +0530

    settings changed

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(pj%x_y2v=v0^=ckl*87d-8(_xyrx)zacp^^*de001v+qidl9v'
+# SECRET_KEY = 'django-insecure-(pj%x_y2v=v0^=ckl*87d-8(_xyrx)zacp^^*de001v+qidl9v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://studentmanagementappcarnot.herokuapp.com/']
 
 
 # Application definition
